e9c49849d3dcc3a23347b41df53af6e4871296a0
bia1708 <bianca-maria.popu@analog.com>
2025-06-12 14:45:02 +0300

    wip
    Signed-off-by: bia1708 <bianca-maria.popu@analog.com>

---
+++ .github/install_deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install build-essential fakeroot libncurses5-dev libssl-dev ccache -y
+sudo apt install dfu-util u-boot-tools device-tree-compiler libssl1.0-dev mtools -y
+sudo apt install bc python3 cpio zip unzip rsync file wget -y
